31c498a1eb668ade89adbebee73a0e7019f4222a
cheech174 <cheech174@outlook.com>
2024-06-17 21:56:23 +0300

    Algorithm count, the task is solved

--- FileName.cpp
+++ FileName.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 void PrintWorryCount(const vector<bool>& is_nervous) {
     // подсчитываем количество элементов в векторе is_nervous, равных true
-
+    count(is_nervous.begin(), is_nervous.end(), true);
     int worry_count = 0;
     for (bool status : is_nervous) {
         if (status) {
