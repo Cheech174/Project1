ca5ffcb37f61ecd17900e786a6578b56d8dde088
cheech174 <cheech174@outlook.com>
2024-06-21 09:00:34 +0300

    Algorithm. Task 1. Done

--- FileName.cpp
+++ FileName.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
+using namespace std; 
 
 int main() {
     int count;
+    vector <pair<string, int>> name_age;
     cin >> count;
     for (int i = 0; i < count; ++i) {
         string name;
         int age;
         cin >> name >> age;
-        // сохраните в вектор пар
+        name_age.push_back(make_pair(name, age));
     }
-
+    sort(name_age.begin(), name_age.end());
+    reverse(name_age.begin(), name_age.end());
+    for (const auto& element : name_age) {
+        cout << element.first << " " << "\n";
+    }
+}
     // выведите только имена в порядке убывания возраста
     // Jack
     // John
     // ...
-}
